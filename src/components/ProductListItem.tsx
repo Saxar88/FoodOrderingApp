afa33d87f43bd2fa518f90f12abda9a2ad636983
Saxar88 <v.sasaran@protonmail.com>
2024-03-11 20:43:10 +0200

    build product details screen

--- src/components/ProductListItem.tsx
+++ src/components/ProductListItem.tsx
@@ -1,7 +1,8 @@
-import { Image, StyleSheet, Text, View } from "react-native";
+import { Image, Pressable, StyleSheet, Text } from "react-native";
 
 import Colors from "../constants/Colors";
 import { Product } from "../types";
+import { Link } from "expo-router";
 
 export const defaultPizzaImage =
 	"https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/default.png";
@@ -12,15 +13,17 @@ type ProductListItemProps = {
 
 const ProductListItem = ({ product }: ProductListItemProps) => {
 	return (
-		<View style={styles.container}>
-			<Image
-				source={{ uri: product.image || defaultPizzaImage }}
-				style={styles.image}
-				resizeMode="contain"
-			/>
-			<Text style={styles.title}>{product.name}</Text>
-			<Text>${product.price}</Text>
-		</View>
+		<Link href={`/menu/${product.id}`} asChild>
+			<Pressable style={styles.container}>
+				<Image
+					source={{ uri: product.image || defaultPizzaImage }}
+					style={styles.image}
+					resizeMode="contain"
+				/>
+				<Text style={styles.title}>{product.name}</Text>
+				<Text>${product.price}</Text>
+			</Pressable>
+		</Link>
 	);
 };
 
