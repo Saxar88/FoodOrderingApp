5eb8b67e8304de4699a4b42efa5cd137fd7693dd
Saxar88 <v.sasaran@protonmail.com>
2024-03-13 10:51:43 +0200

    implement user authentication

--- src/app/(user)/_layout.tsx
+++ src/app/(user)/_layout.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import FontAwesome from "@expo/vector-icons/FontAwesome";
-import { Tabs } from "expo-router";
+import { Redirect, Tabs } from "expo-router";
 
 import Colors from "@/src/constants/Colors";
 import { useColorScheme } from "@/src/components/useColorScheme";
 import { useClientOnlyValue } from "@/src/components/useClientOnlyValue";
+import { useAuth } from "@/src/providers/AuthProvider";
 
 function TabBarIcon(props: {
 	name: React.ComponentProps<typeof FontAwesome>["name"];
@@ -14,7 +15,10 @@ function TabBarIcon(props: {
 }
 
 export default function TabLayout() {
+	const { session } = useAuth();
 	const colorScheme = useColorScheme();
+
+	if (!session) return <Redirect href={"/"} />;
 
 	return (
 		<Tabs
--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -10,6 +10,7 @@ import * as SplashScreen from "expo-splash-screen";
 import { useEffect } from "react";
 
 import { useColorScheme } from "../components/useColorScheme";
+import AuthProvider from "../providers/AuthProvider";
 import CartProvider from "../providers/CartProvider";
 
 export { ErrorBoundary } from "expo-router";
@@ -50,17 +51,19 @@ function RootLayoutNav() {
 
 	return (
 		<ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
-			<CartProvider>
-				<Stack>
-					<Stack.Screen name="(admin)" options={{ headerShown: false }} />
-					<Stack.Screen name="(user)" options={{ headerShown: false }} />
-					<Stack.Screen name="(auth)" options={{ headerShown: false }} />
-					<Stack.Screen
-						name="cart"
-						options={{ title: "Shopping cart", presentation: "modal" }}
-					/>
-				</Stack>
-			</CartProvider>
+			<AuthProvider>
+				<CartProvider>
+					<Stack>
+						<Stack.Screen name="(admin)" options={{ headerShown: false }} />
+						<Stack.Screen name="(user)" options={{ headerShown: false }} />
+						<Stack.Screen name="(auth)" options={{ headerShown: false }} />
+						<Stack.Screen
+							name="cart"
+							options={{ title: "Shopping cart", presentation: "modal" }}
+						/>
+					</Stack>
+				</CartProvider>
+			</AuthProvider>
 		</ThemeProvider>
 	);
 }
